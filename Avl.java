a0b1a1e5fa4cd40eac0043c4a711620365bfd8b5
ahilacondo <ahilacondo@unsa.edu.pe>
2023-06-24 22:42:04 -0500

    Finalizamos con la implementacion de insert(), pero hace falta los metodos balanceTo...

--- Avl.java
+++ Avl.java
@@ -17,7 +17,46 @@ public class Avl<E extends Comparable<E>> {
             res = new Node<E>(x);
             this.height = true;
         } else {
-            
+            int resC = current.getData().compareTo(x);
+            if (resC == 0) {
+                throw new ExceptionNoFound("Elemento ya se encuentra en el arbol");
+            }
+            if (resC < 0) {
+                res.setRight(insert(x, current.getRight()));
+                if (this.height) {
+                    switch (res.getBf()) {
+                        case -1:
+                            res.setBf(0);
+                            this.height = false;
+                            break;
+                        case 0:
+                            res.setBf(1);
+                            break;
+                        case 1: 
+                            res = balanceToLeft(res); //Rotación hacia la izquierda en el nodo "res"
+                            this.height = false;
+                            break;
+                    }
+                }
+
+            } else { //Si es mayor a 0
+                res.setLeft(insert(x, current.getLeft()));
+                if (this.height) {
+                    switch (res.getBf()) {
+                        case 1:
+                            res.setBf(0);
+                            this.height = false;
+                            break;
+                        case 0:
+                            res.setBf(-1);
+                            break;
+                        case -1:
+                            res = balanceToRight(res); //Rotación hacia la derecha en el nodo "res"
+                            this.height = false;
+                            break;
+                    }
+                }
+            }
         }
         return res;
     }
